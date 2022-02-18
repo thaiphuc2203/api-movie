7ec9cc0e418212f115a95fdc0959243edc630478
phucthai2203 <thai.phuc@yoose.com>
2022-02-18 22:28:18 +0700

    init folder movie data'

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import  SearchBar  from '../components/SearchBar/index';
+import  ItemMovie  from '../components/ItemMovie/index';
 
 export default class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <div>
+        <SearchBar />
+        <ItemMovie />
+      </div>
+  );
   }
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,12 @@
 
 import userReducer from '../features/Auth/userSlice';
-
+import movieReducer from '../features/Movie/movieSlice';
 const { configureStore } = require("@reduxjs/toolkit");
 
 
 const rootReducer = {
     user: userReducer,
+    movie: movieReducer,
 }
 
 const store = configureStore({
